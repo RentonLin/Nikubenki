434893b1791f8538ec2823ed8512bcf933605a77
Renton <renton20120426@gmail.com>
2018-01-17 09:59:12 +0800

    add slack hook

--- car_watcher.py
+++ car_watcher.py
@@ -4,6 +4,8 @@ from subprocess import call
 import datetime
 import time
 from PIL import Image
+import requests
+import json
 
 #loop
 while (True):
@@ -63,6 +65,16 @@ while (True):
 		#upload to slack if this is a car
 		print('Car found, upload to slack')
 		#wait at least 2 minutes to refresh again
+		headers = {'Content-Type': 'application/json'}
+		params = {"icon_emoji": ":ghost:",
+			"text": "出车啦~快来上啊(Ahe脸)",
+			"username": "car_watcher_daze"}
+		payload = json.dumps(params)
+		print('payload is ' + payload)
+		
+		response = requests.post('https://hooks.slack.com/services/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', data=payload, headers=headers)
+		print(response, response.status_code, response.text)
+		
 		time_interval = 120
 	else:
 		print('not found')
